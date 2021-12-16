a73b9f57d540e288c9e90b35189b4f7c1e024950
James Petran <jm.ptrn@gmail.com>
2021-12-16 13:21:22 -0600

    html + client.js+ lil css

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
@@ -14,7 +14,7 @@ app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
-})
+});
 
 
 let randomNumber;
